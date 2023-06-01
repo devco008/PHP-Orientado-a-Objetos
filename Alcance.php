9c098c556cb510e8d75b9dd3ade36d0b415258bd
Carlos Ochoa <c.carlosochoa@gmail.com>
2023-06-01 19:17:13 -0300

    Alcance

--- Alcance.php
+++ Alcance.php
@@ -1,7 +1,20 @@
 <?php
-
+/**
+ * Con el alcance se garantiza la integridad de los datos
+ * private : solo elementos de la clase
+ */
 class User {
     // public
     //protected
     // private
+
+    public const PAGINATE = 25;
+
+    public $username;
+    // protected $username
+    // private $password
+
+    public function getUsername(){
+        // code
+    }
 }
